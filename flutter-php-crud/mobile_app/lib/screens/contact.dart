0f03169e2ded9ad50c29969199bac89694e90d49
hamidrehman799 <86568465+hamidrehman799@users.noreply.github.com>
2021-07-02 13:07:47 +0500

    set contact us

--- flutter-php-crud/mobile_app/lib/screens/contact.dart
+++ flutter-php-crud/mobile_app/lib/screens/contact.dart
@@ -1,24 +1,37 @@
-import 'package:flutter/material.dart';
 import 'package:contactus/contactus.dart';
-
+import 'package:flutter/material.dart';
 class contact extends StatelessWidget {
-  const contact({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        bottomNavigationBar: ContactUsBottomAppBar(
+          companyName: 'Hamid Rehman',
+          textColor: Colors.white,
+          backgroundColor: Colors.teal.shade300,
+          email: 'hamidrehman799@gmail.com',
+          // textFont: 'Sail',
+        ),
         backgroundColor: Colors.teal,
         body: ContactUs(
-          logo: AssetImage('images/profile.jpg'),
-          email: 'hamidrehman799@gmail.com',
-          phoneNumber: '+923166250651',
-          website: 'none',
-          githubUserName: 'hamidrehman799',
-        ),
+            cardColor: Colors.white,
+            textColor: Colors.teal.shade900,
+            logo: AssetImage('images/profile.jpg'),
+            email: 'hamidrehman799@gmail.com',
+            companyName: 'hamid Rehman',
+            companyColor: Colors.teal.shade100,
+            phoneNumber: '+923166250651',
+            website: 'none',
+            githubUserName: 'hamidrehman799',
+            linkedinURL:
+            'none',
+            tagLine: 'Flutter Developer',
+            taglineColor: Colors.teal.shade100,
+            twitterHandle: 'none',
+            instagram: 'none',
+            facebookHandle: 'none'),
       ),
     );
   }
 }
-
