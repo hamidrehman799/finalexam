da0df224adbcd000bea85d598b488184994da5e9
hamidrehman799 <86568465+hamidrehman799@users.noreply.github.com>
2021-07-02 12:23:51 +0500

    splash screen

--- flutter-php-crud/mobile_app/lib/splashscreen/splashscreen.dart
+++ flutter-php-crud/mobile_app/lib/splashscreen/splashscreen.dart
@@ -1,2 +1,32 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+
+
+    MaterialApp(
+      home: splashscreen(),
+    )
+  );
+}
+
+class splashscreen extends StatefulWidget {
+  @override
+  _splashscreenState createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return splashscreenState();
+  }
+}
+
+class splashscreenState extends State<splashscreen>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
